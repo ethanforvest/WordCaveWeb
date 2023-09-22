1e41c32e86c914ed931b0a8f4c1507612e23bf79
EthanForvest <91059346+ethanforvest@users.noreply.github.com>
2023-09-22 22:06:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const showButton = document.querySelector(".search-btn");
+const favDialog = document.getElementById("portal");
+
+const API = "https://define.wrdp.app/";
+
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+  document.body.style.filter = 'blur(7px)';
+});
+
+favDialog.addEventListener('close', () => {
+  document.body.style.filter = 'none';
+});
+
+document.addEventListener('click', (e) => {
+  const word = document.querySelector('.header p')
+  if (favDialog.open) {
+    if (e.target === word) {
+      favDialog.close();
+    }
+  }
+});
