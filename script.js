c9f2a27af388153d7eaef1bf27c72f376aa5781e
EthanForvest <91059346+ethanforvest@users.noreply.github.com>
2023-09-27 23:15:46 +0530

    new layout

--- script.js
+++ script.js
@@ -1,77 +1,21 @@
-const showButton = document.querySelector(".search-btn");
-const favDialog = document.getElementById("portal");
-const searchBox = document.getElementById("input");
-const API = "https://define.wrdp.app/";
+const saved = document.querySelector(".saved .saved-container");
+const savedExpand = document.querySelector(".saved-expand");
 
-showButton.addEventListener("click", () => {
-  favDialog.showModal();
-  document.body.style.filter = "blur(7px)";
+const recent = document.querySelector(".recent .recent-container");
+const recentExpand = document.querySelector(".recent-expand");
+
+saved.addEventListener("click", () => {
+    if (savedExpand.style.display === "none") {
+        savedExpand.style.display = "block";
+    } else {
+        savedExpand.style.display = "none";
+    }
 });
 
-favDialog.addEventListener("close", () => {
-  document.body.style.filter = "none";
-  searchBox.focus();
-  resetUI();
-});
-
-function resetUI() {
-  document.querySelector(".gif").innerHTML = "";
-  document.querySelector(".header p").innerHTML = "LOADING";
-  document.querySelector(".meaning").innerHTML = "";
-  document.querySelector(".examples").innerHTML = "";
-  document.querySelectorAll(".player p").forEach(p => p.style.display = "none");
-}
-
-function assembler() {
-  const userInput = searchBox.value;
-  const searchQuery = API + userInput;
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", searchQuery);
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        const response = JSON.parse(this.responseText).Senses[0];
-        const definition = document.createElement('li');
-        definition.innerHTML = response.Definition;
-        const examples = document.createElement('li');
-        examples.innerHTML = response.Examples;
-        
-        document.querySelector(".header p").innerHTML = userInput;
-        document.querySelectorAll(".player p")
-          .forEach(p => p.style.display = "block");
-        document.querySelector(".meaning").appendChild(definition);
-        document.querySelector(".examples").appendChild(examples);
-
-        const defId = response.ID;
-
-        fetch(`https://word-images.cdn-wordup.com/opt/${userInput}/selected.json`)
-          .then((res) => {
-            return res.json();
-          })
-          .then((res) => {
-            const media =  res[defId];
-            const gifImg = document.createElement('img');
-            gifImg.setAttribute("src", media);
-            document.querySelector('.gif').appendChild(gifImg);
-          })
-          .catch((err) => console.log(err, "Couldn't Load the Media"));
-
-      } else {
-        console.log("Something went wrong!");
-      }
+recent.addEventListener("click", () => {
+    if (recentExpand.style.display === "none") {
+        recentExpand.style.display = "block";
+    } else {
+        recentExpand.style.display = "none";
     }
-  };
-  xhr.send();
-}
-
-showButton.addEventListener("click", assembler);
-
-document.addEventListener("click", (e) => {
-  const word = document.querySelector(".header p");
-  if (favDialog.open) {
-    if (e.target === word) {
-      favDialog.close();
-    }
-  }
 });
