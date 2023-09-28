a23fc1bfc6dcdc50121cdf6c672dec3fb3448f32
EthanForvest <91059346+ethanforvest@users.noreply.github.com>
2023-09-28 20:54:38 +0530

    portal backend

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ recent.addEventListener("click", () => {
 
 const showButton = document.querySelector(".search-bar-container");
 const favDialog = document.getElementById("portal");
-const searchBox = document.getElementById("input");
+const searchBox = document.getElementById("search-input");
 const API = "https://define.wrdp.app/";
 
 showButton.addEventListener("keypress", (e) => {
@@ -40,50 +40,59 @@ function resetUI() {
   document.querySelectorAll("#portal .player p").forEach(p => p.style.display = "none");
 }
 
-function assembler() {
-  const userInput = searchBox.value.trim();
-  const searchQuery = API + userInput;
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", searchQuery);
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        const response = JSON.parse(this.responseText).Senses[0];
-        const definition = document.createElement('li');
-        definition.innerHTML = response.Definition;
-        const examples = document.createElement('li');
-        examples.innerHTML = response.Examples;
-        
-        document.querySelector(".header p").innerHTML = userInput;
-        document.querySelectorAll(".player p")
-          .forEach(p => p.style.display = "block");
-        document.querySelector(".meaning").appendChild(definition);
-        document.querySelector(".examples").appendChild(examples);
-
-        const defId = response.ID;
-
-        fetch(`https://word-images.cdn-wordup.com/opt/${userInput}/selected.json`)
-          .then((res) => {
-            return res.json();
-          })
-          .then((res) => {
-            const media =  res[defId];
-            const gifImg = document.createElement('img');
-            gifImg.setAttribute("src", media);
-            document.querySelector('.gif').appendChild(gifImg);
-          })
-          .catch((err) => console.log(err, "Couldn't Load the Media"));
-
-      } else {
-        console.log("Something went wrong!");
-      }
-    }
-  };
-  xhr.send();
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-showButton.addEventListener("click", assembler);
+function assembler(e) {
+  if (e.key === "Enter") {
+    const userInput = searchBox.value.trim();
+    const searchQuery = API + userInput;
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", searchQuery);
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          const response = JSON.parse(this.responseText).Senses[0];
+          const definition = document.createElement('li');
+          definition.innerHTML = response.Definition;
+          const examples = document.createElement('li');
+          examples.innerHTML = response.Examples;
+          
+          document.querySelector(".header p").innerHTML = capitalize(userInput);
+          document.querySelectorAll(".player p")
+            .forEach(p => p.style.display = "block");
+          document.querySelector(".meaning").appendChild(definition);
+          document.querySelector(".examples").appendChild(examples);
+
+          const defId = response.ID;
+
+          fetch(`https://word-images.cdn-wordup.com/opt/${userInput}/selected.json`)
+            .then((res) => {
+              return res.json();
+            })
+            .then((res) => {
+              const media =  res[defId];
+              const gifImg = document.createElement('img');
+              gifImg.setAttribute("src", media);
+              document.querySelector('.gif').appendChild(gifImg);
+            })
+            .catch((err) => console.log(err, "Couldn't Load the Media"));
+
+        } else {
+          console.log("Something went wrong!");
+        }
+      }
+    };
+
+    xhr.send();
+
+    favDialog.addEventListener("close", () => xhr.abort());
+  }
+}
+
+showButton.addEventListener("keypress", assembler);
 
 document.addEventListener("click", (e) => {
   const word = document.querySelector(".header p");
